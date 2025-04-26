fe7bf040e7ada540e314ad6de1043c5af0ef26d2
desouzawizard <desouzawizard@gmail.com>
2025-04-25 21:19:44 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
+function showContent(id) {
+        // Hide all content sections
+        document
+          .querySelectorAll(".content")
+          .forEach((div) => div.classList.remove("active"))
+        // Show the selected content section
+        document.getElementById(id).classList.add("active")
+      }
+      function toggleFAQ(item) {
+        item.classList.toggle("active")
+      }
 
+      function submitFeedback() {
+        alert("Thank you for your feedback!")
+      }
+      function toggleDetails(id) {
+        var element = document.getElementById(id)
+        element.style.display =
+          element.style.display === "none" ? "block" : "none"
+      }
